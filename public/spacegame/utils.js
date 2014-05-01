3a73f3babd72af04b9a10eece9bdf3546bcfde9c
hammadmlk <hammadmlk@gmail.com>
2014-05-01 07:11:47 -0400

    add getJson function

--- public/spacegame/utils.js
+++ public/spacegame/utils.js
@@ -5,6 +5,26 @@ define(function () {
 		},
 		func2 : function () {
 			alert('f2')
-		}
+		},
+    getJson: function(url, callback){
+    //open url with GET. Convert response to JSON n return it.
+    //callback(err, json)
+      xmlhttp = new XMLHttpRequest();
+			xmlhttp.onreadystatechange = function () {
+				if (xmlhttp.readyState == 0) {}
+				if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+					//success
+					var res = xmlhttp.responseText;
+					var resJson = JSON.parse(res);
+					callback(null, resJson);
+				} else if (xmlhttp.readyState == 4) {
+					//fail
+					callback('error loading json '+url);
+				}
+			}
+			xmlhttp.open("GET", url, false);
+			xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+			xmlhttp.send();
+    }
 	}
 });
