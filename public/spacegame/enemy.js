5b529608e352ba4444349d0902aa9557c4f4677a
hammadmlk <hammadmlk@gmail.com>
2014-05-01 07:23:44 -0400

    variable color and size option

--- public/spacegame/enemy.js
+++ public/spacegame/enemy.js
@@ -4,19 +4,31 @@
 define(["globals"], function (GLOBAL) {
 	return function (I) {
 		I = I || {};
-
+    
+    
+    if (typeof I.a==='undefined'){
+      I.color = "#A2B";
+      I.width = 32;
+      I.height = 32;
+    }
+    else{
+      I.color = I.color;
+      I.width = 32*I.size;
+      I.height = 32*I.size;
+    }
+    
+    
 		I.active = true;
 		I.age = Math.floor(Math.random() * 128);
 
-		I.color = "#A2B";
+		
 
 		I.x = GLOBAL.CANVAS_WIDTH / 4 + Math.random() * GLOBAL.CANVAS_WIDTH / 2;
 		I.y = 0;
-		I.xVelocity = 0
+		I.xVelocity = 0;
 			I.yVelocity = 2;
 
-		I.width = 32;
-		I.height = 32;
+
 
 		I.inBounds = function () {
 			return I.x >= 0 && I.x <= GLOBAL.CANVAS_WIDTH &&
@@ -26,7 +38,8 @@ define(["globals"], function (GLOBAL) {
 		I.sprite = GLOBAL.SPRITE("enemy");
 
 		I.draw = function () {
-			this.sprite.draw(GLOBAL.CANVAS, this.x, this.y);
+      this.sprite.fill(GLOBAL.CANVAS, this.x, this.y, this.width, this.height, '',this.color)
+			//this.sprite.draw(GLOBAL.CANVAS, this.x, this.y);
 		};
 
 		I.update = function () {
