abe8495952aa518d95c063382099836215da8d49
hammadmlk <hammadmlk@gmail.com>
2014-05-01 07:11:06 -0400

    print EDT hour

--- public/spacegame/view.js
+++ public/spacegame/view.js
@@ -11,10 +11,20 @@ define(["globals", "enemy"], function (GLOBAL, Enemy) {
 			GLOBAL.ENEMIES.forEach(function (enemy) {
 				enemy.draw();
 			});
-
+      
+      var edttime = GLOBAL.GAMEHOUR-4
+      if(edttime<0){edttime = 24+edttime}
+      
 			GLOBAL.CANVAS.font = "30px Arial";
-			GLOBAL.CANVAS.fillText("This HOUR: " + GLOBAL.GAMEHOUR, 10, 50);
-			GLOBAL.CANVAS.font = "20px Arial";
+			//GLOBAL.CANVAS.fillText("This HOUR: " + GLOBAL.GAMEHOUR, 10, 50);
+			GLOBAL.CANVAS.fillText("This HOUR: " + edttime + " EDT", 10, 50);
+			
+      GLOBAL.CANVAS.font = "20px Arial";
+      
+      if(GLOBAL.OUTGOINGEMAILDATA[GLOBAL.GAMEHOUR]==undefined){
+        GLOBAL.OUTGOINGEMAILDATA[GLOBAL.GAMEHOUR]=[];
+        console.log('empty')
+      }
 			GLOBAL.CANVAS.fillText("   Bullets Remaining: " + GLOBAL.OUTGOINGEMAILDATA[GLOBAL.GAMEHOUR].length, 10, 75);
       GLOBAL.CANVAS.font = "13px Arial";
 			GLOBAL.CANVAS.fillText("      Enemy Number: " + GLOBAL.HOURENEMYNUMBER, GLOBAL.CANVAS_WIDTH - 150, 50);
