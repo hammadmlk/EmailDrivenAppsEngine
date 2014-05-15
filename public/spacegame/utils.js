5a58c45fe071008eba42a382cc6083e3d24cebe7
hammadmlk <hammadmlk@gmail.com>
2014-05-15 06:17:58 -0400

    new utlis

--- public/spacegame/utils.js
+++ public/spacegame/utils.js
@@ -33,6 +33,23 @@ define(function () {
 			} else {
 				return true;
 			}
+		},
+		getCookie : function (cname) {
+			var name = cname + "=";
+			var ca = document.cookie.split(';');
+			for (var i = 0; i < ca.length; i++) {
+				var c = ca[i].trim();
+				if (c.indexOf(name) == 0)
+					return c.substring(name.length, c.length);
+			}
+			return "";
+		},
+		setCookie : function (cname, cvalue, exmins) {
+			var d = new Date();
+			d.setTime(d.getTime() + (exmins * 60 * 1000));
+			var expires = "expires=" + d.toGMTString();
+			var domain = "path=/;"
+				document.cookie = cname + "=" + cvalue + "; " + domain + expires;
 		}
 	}
 });
