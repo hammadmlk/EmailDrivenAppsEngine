6b4dc2741ef8279f624190920111e82b81857233
hammadmlk <hammadmlk@gmail.com>
2014-05-18 16:53:08 -0400

    enemy all direction support

--- public/spacegame/enemy.js
+++ public/spacegame/enemy.js
@@ -9,10 +9,10 @@ define(["globals"], function (GLOBAL) {
       I.color = "grey";
     }
     if (typeof I.width==='undefined'){
-      I.width = 75;
+      I.width = 47;
     }
     if (typeof I.height==='undefined'){
-      I.height = 40;
+      I.height = 25;
     }
     if (typeof I.scale !== 'undefined'){
       I.width = I.width * I.scale;
@@ -21,16 +21,38 @@ define(["globals"], function (GLOBAL) {
     
     I.active = true;
 		I.age = Math.floor(Math.random() * 128);
-
-
-		I.x = GLOBAL.CANVAS_WIDTH / 4 + Math.random() * GLOBAL.CANVAS_WIDTH / 2;
-		I.y = -I.height;
-		I.xVelocity = 0;
-	  I.yVelocity = 1;
-
+    
+    //== Set x, y velocity     
+    if(GLOBAL.GAMEMODE=='leftrightonly'){
+      I.x = GLOBAL.CANVAS_WIDTH / 4 + Math.random() * GLOBAL.CANVAS_WIDTH / 2;
+      I.y = -I.height;
+      I.xVelocity = 0;
+      I.yVelocity = 1;
+      I.angle = 0;
+    }else{
+      //start pos of enemy
+      I.x = Math.random() * GLOBAL.CANVAS_WIDTH ;
+      I.y = Math.random() * GLOBAL.CANVAS_HEIGHT;
+      if (Math.random() < 0.5) {
+        if (Math.random() < 0.5) { I.x = -I.width } else {I.x = GLOBAL.CANVAS_WIDTH+I.width;}
+      } else { 
+        if (Math.random() < 0.5) {	I.y = -I.height } else { I.y = GLOBAL.CANVAS_HEIGHT+I.height; }
+      }
+      
+      //angle
+      var cy = GLOBAL.CANVAS_HEIGHT/2;
+      var cx = GLOBAL.CANVAS_WIDTH/2;
+      I.angle = Math.PI/2 + Math.atan2(cy - I.y, cx - I.x);// - atan2(GLOBAL.CANVAS_HEIGHT/2, GLOBAL.CANVAS_WIDTH/2);
+      I.angle = I.angle + (Math.random()*Math.PI/2-Math.PI/4);
+      
+      //velocity based on angle
+      I.xVelocity = Math.sin(I.angle);
+      I.yVelocity = -Math.cos(I.angle);
+    }
+    //======
 
 		I.inBounds = function () {
-			return I.x >= 0 && I.x <= GLOBAL.CANVAS_WIDTH &&
+			return I.x >= -I.width && I.x <= GLOBAL.CANVAS_WIDTH+I.width &&
         I.y >= -I.height && I.y <= GLOBAL.CANVAS_HEIGHT+I.height;
 		};
 
@@ -40,15 +62,17 @@ define(["globals"], function (GLOBAL) {
       //this.sprite.fill(GLOBAL.CANVAS, this.x, this.y, this.width, this.height, '',this.color)
 			//this.sprite.fill(GLOBAL.CANVAS, this.x, this.y, this.width, this.height, '');
       //this.sprite.draw(GLOBAL.CANVAS, this.x, this.y);
-      this.sprite.draw(GLOBAL.CANVAS, this.x, this.y, this.width, this.height);
+      this.sprite.draw(GLOBAL.CANVAS, this.x, this.y, this.width, this.height, this.angle);
+    
 		};
 
 		I.update = function () {
 			I.x += I.xVelocity;
 			I.y += I.yVelocity;
-
-			I.xVelocity = /*0;*/ Math.random() * 0.5 * Math.sin(I.age * Math.PI / 64);
-
+      
+      if(GLOBAL.GAMEMODE=='leftrightonly'){
+        I.xVelocity = /*0;*/ Math.random() * 0.5 * Math.sin(I.age * Math.PI / 64);
+      }
 			I.age++; //why is it here?
 
 			I.active = I.active && I.inBounds();
